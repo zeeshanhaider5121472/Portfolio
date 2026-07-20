7e7c90897b1f5d0540d5101c5487afc7e28ea162
zeeshan Haider <shani721287@gmail.com>
2026-07-20 21:09:48 +0500

    blogs removed

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -6,27 +6,28 @@ import React from "react";
 
 export default function Home(): React.JSX.Element {
   const designs = [
-    {
-      title: "How to build Shopping Cart using HTML, CSS and Js.",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/upload/v1662211619297/WHIzah9m5.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/making-a-shopping-cart",
-      id: "1",
-    },
-    {
-      title: "JavaScript Interview Question (Directory Mapping 📂)",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/4iN-LAzqAdE/upload/8491114c3ac15ca04606ee141e1fd598.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/js-interview-ques-directory-mapping",
-      id: "2",
-    },
-    {
-      title: "Oracle Interview Experience (On-Campus)",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/upload/v1691046429941/a65b7643-50d5-4433-a3a7-890b854ce137.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/oracle-interview-experience-on-campus",
-      id: "3",
-    },
+    {},
+    // {
+    //   title: "How to build Shopping Cart using HTML, CSS and Js.",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1662211619297/WHIzah9m5.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/making-a-shopping-cart",
+    //   id: "1",
+    // },
+    // {
+    //   title: "JavaScript Interview Question (Directory Mapping 📂)",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/4iN-LAzqAdE/upload/8491114c3ac15ca04606ee141e1fd598.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/js-interview-ques-directory-mapping",
+    //   id: "2",
+    // },
+    // {
+    //   title: "Oracle Interview Experience (On-Campus)",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1691046429941/a65b7643-50d5-4433-a3a7-890b854ce137.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/oracle-interview-experience-on-campus",
+    //   id: "3",
+    // },
   ];
 
   return (
--- src/components/Homepage/UI/UX/Designs.tsx
+++ src/components/Homepage/UI/UX/Designs.tsx
@@ -1,30 +1,30 @@
 import Heading from "@/components/Heading";
 import Link from "next/link";
-import DesignCard from "./DesignCard";
 
 const Designs = () => {
   const designs = [
-    {
-      title: "How to build Shopping Cart using HTML, CSS and Js.",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/upload/v1662211619297/WHIzah9m5.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/making-a-shopping-cart",
-      id: "1",
-    },
-    {
-      title: "JavaScript Interview Question (Directory Mapping 📂)",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/4iN-LAzqAdE/upload/8491114c3ac15ca04606ee141e1fd598.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/js-interview-ques-directory-mapping",
-      id: "2",
-    },
-    {
-      title: "Oracle Interview Experience (On-Campus)",
-      image:
-        "https://cdn.hashnode.com/res/hashnode/image/upload/v1691046429941/a65b7643-50d5-4433-a3a7-890b854ce137.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-      link: "https://sudhanshuranjanblogs.hashnode.dev/oracle-interview-experience-on-campus",
-      id: "3",
-    },
+    {},
+    // {
+    //   title: "How to build Shopping Cart using HTML, CSS and Js.",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1662211619297/WHIzah9m5.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/making-a-shopping-cart",
+    //   id: "1",
+    // },
+    // {
+    //   title: "JavaScript Interview Question (Directory Mapping 📂)",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/4iN-LAzqAdE/upload/8491114c3ac15ca04606ee141e1fd598.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/js-interview-ques-directory-mapping",
+    //   id: "2",
+    // },
+    // {
+    //   title: "Oracle Interview Experience (On-Campus)",
+    //   image:
+    //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1691046429941/a65b7643-50d5-4433-a3a7-890b854ce137.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    //   link: "https://sudhanshuranjanblogs.hashnode.dev/oracle-interview-experience-on-campus",
+    //   id: "3",
+    // },
   ];
 
   return (
@@ -34,7 +34,7 @@ const Designs = () => {
         emoji="🎨"
         desc="I also create cool designs."
       />
-      <div className="flex my-16 flex-wrap items-center justify-evenly gap-6 gap-y-16">
+      {/* <div className="flex my-16 flex-wrap items-center justify-evenly gap-6 gap-y-16">
         {designs.map((design, idx) => (
           <DesignCard
             title={design.title}
@@ -44,7 +44,7 @@ const Designs = () => {
             link={design.link}
           />
         ))}
-      </div>
+      </div> */}
       <div className="flex items-center justify-center">
         <Link href="/designs">
           <button className="custom-button m-auto">View All</button>
