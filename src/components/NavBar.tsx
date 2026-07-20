0485bbcb8f384f3a1ae0027c73bcc9a0d276a301
zeeshan Haider <shani721287@gmail.com>
2026-07-20 20:26:07 +0500

    logo fixed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,87 +1,140 @@
-import { Oswald } from 'next/font/google'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { HiOutlineMenuAlt4 } from 'react-icons/hi'
-import { RxCross2 } from 'react-icons/rx'
-const oswald = Oswald({ subsets: ['latin'] })
-
+import { Oswald } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
+const oswald = Oswald({ subsets: ["latin"] });
 
 const NavBar = () => {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    return (
-        <>
-            <div className='fixed lg:top-10 md:top-8 top-6 w-[90%] m-auto z-[769]'>
-                <div className='flex self-center justify-between items-center lg:w-[55%] md:w-[80%] m-auto rounded-3xl p-3 px-5 border-[1.5px] border-gray-800 bg-slate-800 bg-opacity-50 backdrop-blur-lg'>
-                    <Link className='flex items-center' href="/">
-                        <Image className='h-6 w-9' height={300} width={300} alt="logo" src="/images/z.png" />
-                        <p className='text-gray-300'>
-                            <span className={oswald.className}>Haider</span>
-                        </p>
-                    </Link>
+  return (
+    <>
+      <div className="fixed lg:top-10 md:top-8 top-6 w-[90%] m-auto z-[769]">
+        <div className="flex self-center justify-between items-center lg:w-[55%] md:w-[80%] m-auto rounded-3xl p-3 px-5 border-[1.5px] border-gray-800 bg-slate-800 bg-opacity-50 backdrop-blur-lg">
+          <Link className="flex items-center gap-2" href="/">
+            <Image
+              className="h-6 w-6"
+              height={300}
+              width={300}
+              alt="logo"
+              src="/images/z.png"
+            />
+            <p className="text-gray-300">
+              <span className={oswald.className}>Haider</span>
+            </p>
+          </Link>
 
-                    <div onClick={() => setOpen(!open)} className='flex items-center gap-2 p-2 cursor-pointer'>
-                        <div className='text-gray-300'>
-                            <span className={oswald.className}>
-                                {
-                                    open ? "CLOSE" : "MENU"
-                                }
-                            </span>
-                        </div>
-                        <button>
-                            {open ? <RxCross2 className='text-gray-400 text-2xl' /> : <HiOutlineMenuAlt4 className='text-gray-400 text-2xl' />}
-                        </button>
-                    </div>
-                </div>
+          <div
+            onClick={() => setOpen(!open)}
+            className="flex items-center gap-2 p-2 cursor-pointer"
+          >
+            <div className="text-gray-300">
+              <span className={oswald.className}>
+                {open ? "CLOSE" : "MENU"}
+              </span>
             </div>
+            <button>
+              {open ? (
+                <RxCross2 className="text-gray-400 text-2xl" />
+              ) : (
+                <HiOutlineMenuAlt4 className="text-gray-400 text-2xl" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
 
-            <nav style={{
-                transform: open ? 'translateX(0%)' : 'translateX(-100%)',
-                transition: 'transform 0.6s ease-in-out',
-            }} className="bg-[#141421] fixed inset-0 w-full opa h-screen z-[768] flex justify-evenly items-center">
-                <div className="flex flex-col gap-16">
-                    <div className='lg:text-6xl md:text-6xl text-5xl font-bold'>
-                        <Link href="/" onClick={() => setOpen(!open)} className='text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer'>Home</Link>
-                    </div>
-                    <div className='lg:text-6xl md:text-6xl text-5xl font-bold'>
-                        <Link href="/projects" onClick={() => setOpen(!open)} className='text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer'>Projects</Link>
-                    </div>
-                    <div className='lg:text-6xl md:text-6xl text-5xl font-bold'>
-                        <Link href="/designs" onClick={() => setOpen(!open)} className='text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer'>Designs</Link>
-                    </div>
-                </div>
-                <div className='lg:flex md:flex hidden flex-col items-center justify-center gap-28'>
-                    <div className={oswald.className}>
+      <nav
+        style={{
+          transform: open ? "translateX(0%)" : "translateX(-100%)",
+          transition: "transform 0.6s ease-in-out",
+        }}
+        className="bg-[#141421] fixed inset-0 w-full opa h-screen z-[768] flex justify-evenly items-center"
+      >
+        <div className="flex flex-col gap-16">
+          <div className="lg:text-6xl md:text-6xl text-5xl font-bold">
+            <Link
+              href="/"
+              onClick={() => setOpen(!open)}
+              className="text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="lg:text-6xl md:text-6xl text-5xl font-bold">
+            <Link
+              href="/projects"
+              onClick={() => setOpen(!open)}
+              className="text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer"
+            >
+              Projects
+            </Link>
+          </div>
+          <div className="lg:text-6xl md:text-6xl text-5xl font-bold">
+            <Link
+              href="/designs"
+              onClick={() => setOpen(!open)}
+              className="text-transparent font-bold font-outline-white hover:text-purple-500 cursor-pointer"
+            >
+              Designs
+            </Link>
+          </div>
+        </div>
+        <div className="lg:flex md:flex hidden flex-col items-center justify-center gap-28">
+          <div className={oswald.className}>
+            <div className="text-center">
+              <h2 className="text-xl text-gray-400 font-semibold mb-5">
+                {" "}
+                DON&apos;T BE A STRANGER
+              </h2>
 
-                        <div className='text-center'>
-                            <h2 className='text-xl text-gray-400 font-semibold mb-5'> DON&apos;T BE A STRANGER</h2>
+              <div className="flex gap-4 text-2xl font-bold">
+                <Link
+                  className="text-sky-400 hover:text-white"
+                  href="https://github.com/zeeshanhaider5121472/"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  className="text-sky-400 hover:text-white"
+                  href="https://vercel.com/zeeshanhaider5121472s-projects"
+                >
+                  Vercel
+                </Link>
+                <Link
+                  className="text-sky-400 hover:text-white"
+                  href="https://www.linkedin.com/in/zeeshan-haider-80b50a179/"
+                >
+                  LinkedIn
+                </Link>
+                <Link
+                  className="text-sky-400 hover:text-white"
+                  href="https://instagram.com/"
+                >
+                  Instagram
+                </Link>
+              </div>
+            </div>
+          </div>
 
-                            <div className='flex gap-4 text-2xl font-bold'>
-                                <Link className='text-sky-400 hover:text-white' href="https://github.com/zeeshanhaider5121472/">GitHub</Link>
-                                <Link className='text-sky-400 hover:text-white' href="https://vercel.com/zeeshanhaider5121472s-projects">Vercel</Link>
-                                <Link className='text-sky-400 hover:text-white' href="https://www.linkedin.com/in/zeeshan-haider-80b50a179/">LinkedIn</Link>
-                                <Link className='text-sky-400 hover:text-white' href="https://instagram.com/">Instagram</Link>
-                            </div>
-                        </div>
+          <div className="text-center">
+            <div className={oswald.className}>
+              <p className="text-gray-400 text-lg font-medium mb-5">
+                HAVE AN IDEA?
+              </p>
+            </div>
+            <Link href="mailto:Shani721287@gmail.com" target="_blank">
+              <h3 className="text-5xl font-bold hover:text-purple-400">
+                Tell me about it
+              </h3>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-                    </div>
-
-                    <div className='text-center'>
-                        <div className={oswald.className}>
-                            <p className='text-gray-400 text-lg font-medium mb-5'>HAVE AN IDEA?</p>
-                        </div>
-                        <Link href="mailto:Shani721287@gmail.com" target='_blank'>
-                            <h3 className='text-5xl font-bold hover:text-purple-400'>
-                                Tell me about it
-                            </h3>
-                        </Link>
-                    </div>
-                </div>
-
-            </nav>
-        </>
-    )
-}
-
-export default NavBar
+export default NavBar;
